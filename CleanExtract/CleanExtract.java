39431f5d88f94a72aa5625ec2c501754e9f017c0
mdinani <4minedina@gmail.com>
2025-09-23 17:11:29 +0100

    CleanExtract

--- CleanExtract/CleanExtract.java
+++ CleanExtract/CleanExtract.java
@@ -1,7 +1,39 @@
 public class CleanExtract {
     public static String extract(String s) {
-        for (int i=0; i < s.length(); i++) {
-            
+        String[] parts = s.split("\\|");
+        StringBuilder result = new StringBuilder();
+
+        for (String part : parts) {
+            part = part.trim(); 
+            int firstpoint = part.indexOf('.');
+            // System.out.println("first dot: " + firstpoint);
+            int lastpoint = part.lastIndexOf('.');
+            // System.out.println("SECOND dot: " + lastpoint);
+
+            String temp = "";
+
+            if (firstpoint != -1 && lastpoint != -1 && firstpoint < lastpoint) {
+                
+                temp = part.substring(firstpoint + 1, lastpoint).trim();
+            } else if (firstpoint != -1) {
+                
+                temp = part.substring(firstpoint + 1).trim();
+            } else if (firstpoint == -1 && lastpoint == -1) {
+                
+                temp = part;
+            } else {
+                
+                temp = part.trim();
+            }
+
+            if (!temp.isEmpty()) {
+                if (result.length() > 0) {
+                    result.append(" ");
+                }
+                result.append(temp);
+            }
         }
+
+        return result.toString();
     }
 }
